8a7e781f124bbc851fcf5ab6c916d31d0e42b36a
K Selatlha <222507723@tut4life.ac.za>
2025-08-20 13:24:04 +0200

    updated app file

---
+++ app.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const fs = require('fs');
+const port = 3000;
+
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  fs.readFile('index.html', (err, data) => {
+  if (err) {
+      res.writeHead(404);
+      res.write('404 Not Found');
+    } else {
+      res.write( data );
+
+      }
+       res.end()
+  })
+   
+});
+
+server.listen(port, (error) => {
+  if(error) {
+    console.log(`Error starting server:` ,error)
+  }else {
+        console.log(`Server is listening on port:` +port )
+    }
+    
+
+});
